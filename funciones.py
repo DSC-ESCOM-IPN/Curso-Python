e44ab5e98288524383f4b54f38cca9a61d5997d0
JoseRivera12 <jose.rivera@cornershopapp.com>
2021-09-05 19:27:05 -0500

    added clase 1

---
+++ funciones.py
@@ -0,0 +1,22 @@
+def suma_dos_numeros(num1: int, num2: int) -> str:
+    return 2.3
+
+def imprime_numeros(n: int):
+    for i in range(0, 100):
+        if i % 2 == 0:
+            print(i)
+        else:
+            print(1)
+    print([i if i % 2 == 0 else 1 for i in range(100)])
+
+
+# print(suma_dos_numeros(3, 1) + 1.2)
+
+# imprime_numeros(1)
+list1 = [1,2,3,4,5]
+dict1 = dict()
+cadena = ""
+numero = 0 
+numero = 0.0
+list1.append(6)
+print(list1)
